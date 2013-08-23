901b67c0a356c023ed36ea3a5d23d87bb8f6a002
Aaron Cooley <aaron@kineticanalysis.com>
2013-08-22 21:21:26 -0700

    Adding a rest API for the RainForrest gateway to call

--- app/controllers/meter_reading_controller.rb
+++ app/controllers/meter_reading_controller.rb
@@ -1,27 +1,17 @@
 require 'net/https'
 require 'uri'
+require 'nokogiri'
 
 class MeterReadingController < ApplicationController
   def index
-    # Nothing dynamic for now, just get the meter reading from Max's home...
-    url = URI.encode('http://my.idigi.com/ws/XbeeAttributeDataFull?condition=xeEndpointId= 1 and xcClusterType= 0 and xcClusterId= 1794 and xaAttributeId= 1024')
-    username = 'slalomdigital'
-    password = 'Mogu2l4m!'
+    # Todo: Capture the data from the meter and put in in to the database...
+    doc = Nokogiri::XML(params['body'])
 
-    uri = URI.parse(url)
+    meterMacId = doc.xpath("//MeterMacId").first.content
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    # I was going to use HTTPS, but it's F__king way to complicated to do it right.
-    #http.use_ssl = true
-    #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.basic_auth(username, password)
-    request['Accept'] = 'application/json'
-    response = http.request(request)
 
     respond_to do |format|
-      format.json { render json: response.body }
+      format.xml { render status: :ok }
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,99 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130823040850) do
+
+  create_table "current_summations", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "meter_mac_id"
+    t.datetime "timestamp"
+    t.integer  "summation_delivered"
+    t.integer  "summation_received"
+    t.integer  "multiplier"
+    t.integer  "divisor"
+    t.integer  "digits_right"
+    t.integer  "digits_left"
+    t.boolean  "suppress_leading_zero"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  create_table "device_infos", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "install_code"
+    t.string   "firmware_version"
+    t.string   "hardware_version"
+    t.string   "manufacturer"
+    t.string   "model_id"
+    t.string   "date_code"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "instantaneous_demands", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "meter_mac_id"
+    t.datetime "timestamp"
+    t.integer  "demand"
+    t.integer  "multiplier"
+    t.integer  "divisor"
+    t.integer  "digits_right"
+    t.integer  "digits_left"
+    t.boolean  "suppress_leading_zero"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  create_table "messages", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "meter_mac_id"
+    t.datetime "timestamp"
+    t.string   "text"
+    t.boolean  "confirmation_required"
+    t.boolean  "confirmed"
+    t.integer  "queue"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  create_table "meter_infos", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "meter_mac_id"
+    t.integer  "meter_type"
+    t.string   "nick_name"
+    t.string   "account"
+    t.string   "auth"
+    t.string   "host"
+    t.boolean  "enabled"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "network_infos", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "coordinator_mac_id"
+    t.integer  "status"
+    t.string   "description"
+    t.integer  "status_code"
+    t.integer  "extended_pan_id"
+    t.integer  "channel"
+    t.integer  "short_address"
+    t.integer  "link_strength"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "price_clusters", :force => true do |t|
+    t.integer  "device_mac_id"
+    t.integer  "meter_mac_id"
+    t.datetime "timestamp"
+    t.integer  "price"
+    t.integer  "currency"
+    t.integer  "trailing_digits"
+    t.integer  "tier"
+    t.string   "rate_label"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
